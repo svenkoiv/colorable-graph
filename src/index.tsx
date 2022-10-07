12cb44f142f0a445a1887bb240b9ab9b0fae36a7
Sven Kõiv <>
2022-10-07 22:32:46 +0300

    project setup

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    Hello world
+  </React.StrictMode>,
+  rootElement
+);
